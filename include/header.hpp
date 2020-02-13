032925a436809cd1536a0a27af36caabfd56b135
Darioshka <56868583+Darioshka@users.noreply.github.com>
2020-02-13 23:34:43 +0300

    1.5

--- include/header.hpp
+++ include/header.hpp
@@ -7,16 +7,14 @@
 
 template <typename T>
 class Stack{
-	
 private:
     T ArrStack[30];
     int top;
-    
+
 public:
     Stack(){
         top = 0;
     }
-
     void push(T&& value){
         if (top < 30){
             ArrStack[top] = value;
@@ -29,13 +27,11 @@ public:
             top++;
         }
     }
-	
     void pop(){
         if (top > 0){
             ArrStack[--top];
         }
     }
-    
     const T& head() const{
         return ArrStack[top-1];
     }
@@ -52,29 +48,26 @@ public:
     stack(){
         top = 0;
     }
-	
     template <typename ... Args>
     void push_emplace(Args&&... value){
         T obj(std::forward<Args>(value)...);
-        if(top < 30) {
+        if (top < 30) {
             ArrStack[top] = obj;
             top++;
         }
     }
-	
     void push(T&& value){
-        if(top < 30) {
+        if (top < 30) {
             ArrStack[top] = value;
             top++;
         }
     }
-	
     const T& head() const{
         return ArrStack[top-1];
     }
-	
+    
     T pop(){
-        if(top > 0){
+        if (top > 0){
             T rezult = ArrStack[top-1];
             ArrStack[--top];
             return rezult;
