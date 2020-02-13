9ab7985548602436591e4a1e03835db6839b82ed
Darioshka <56868583+Darioshka@users.noreply.github.com>
2020-02-13 22:53:32 +0300

    1.1

--- include/header.hpp
+++ include/header.hpp
@@ -2,5 +2,43 @@
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
+#include <iostream>
+#include <cstring>
+using namespace std;
+template <typename T>
+class Stack{
+private:
+    T ArrStack[30];
+    int top;
+public:
+    Stack(){
+        top = 0;
+    }
 
+
+    void push(T&& value){
+        if(top < 30) {
+            ArrStack[top] = value;
+            top++;
+        }
+
+    }
+    void push(const T& value){
+        if(top < 30) {
+            ArrStack[top] = value;
+            top++;
+        }
+
+
+    }
+    void pop(){
+        if(top > 0){
+            ArrStack[--top];
+        }
+
+    }
+    const T& head() const{
+        return ArrStack[top-1];
+    }
+};
 #endif // INCLUDE_HEADER_HPP_
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,25 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <gtest/gtest.h>
+#include <header.hpp>
+#include <type_traits>
 
 TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+
+    Stack<int> Stack_1;
+    int var_1 = 6;
+    int var_2 = 8;
+    int var_3 = 10;
+    Stack_1.push(var_1);
+    Stack_1.push(var_2);
+    Stack_1.pop();
+    Stack_1.push(var_3);
+
+    EXPECT_EQ(static_cast<int>(Stack_1.head()), 10);
+    
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
